46e6c74a07cfaca329e16a0438750c3681b7834e
Rodrigo Ruben <RomanRodrigoRuben@hotmail.com>
2024-01-18 15:42:09 -0300

    Prueba

---
+++ MauiApp2/Shared/Scanner/RecepcionCajaParamDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace bh001_camera_barcode.Shared.Scanner
+{
+    public class RecepcionCajaParamDTO
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string NroCaja { get; set; } = string.Empty;
+    }
+}
+
+
